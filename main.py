30d1b4fef6344a526f4379b48056bc8287d939ff
marcosdayanm <marcosdayanmann@gmail.com>
2024-02-29 13:20:07 -0600

    Multiple games in runtime feature with exit option

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 import random
 
 def adivinar_numero():
-    numero_secreto = random.randint(1, 10)
-    intentos = 0
 
     while True:
-        intento = int(input("Adivina el número (entre 1 y 10): "))
-        intentos += 1
+        if input("Salir? (y/n): ").lower() == "y":
+            break;
 
-        if intento < numero_secreto:
-            print("Demasiado bajo. Intenta de nuevo.")
-        elif intento > numero_secreto:
-            print("Demasiado alto. Intenta de nuevo.")
-        else:
-            print(f"¡Felicidades! Adivinaste el número en {intentos} intentos.")
-            break
+        numero_secreto = random.randint(1, 10)
+        intentos = 0
+
+        while True:
+            intento = int(input("Adivina el número (entre 1 y 10): "))
+            intentos += 1
+
+            if intento < numero_secreto:
+                print("Demasiado bajo. Intenta de nuevo.")
+            elif intento > numero_secreto:
+                print("Demasiado alto. Intenta de nuevo.")
+            else:
+                print(f"¡Felicidades! Adivinaste el número en {intentos} intentos.")
+                break
 
 adivinar_numero()
 
